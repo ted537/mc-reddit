64704a3c720071de63b82975f234cc1e11734b0d
Ted Brownlow <ted.brownlow@gmail.com>
2020-07-08 19:35:37 -0400

    fixed crash when no server infos ready

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,13 @@ fakeServerInfos(num_posts).then(infos=>server_infos=infos);
 const refresh_counts={};
 const servers = ports().map(port=>{
     const server = fakeMcServer(client=>{
+        if (server_infos.length===0) {
+            return {
+                text:"loading...",
+                players:0,
+                max_players:0
+            }
+        }
         refresh_counts[client] = refresh_counts[client] || 0;
         const refresh_count = refresh_counts[client];
         const index = refresh_count%server_infos.length
